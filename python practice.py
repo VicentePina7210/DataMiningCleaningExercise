88e17060af47aaad64887adb07652894265cbcbf
VicentePina7210 <vpina3210@gmail.com>
2024-09-04 21:21:44 -0500

    first commit

---
+++ python practice.py
@@ -0,0 +1,28 @@
+# python  has three numeric types and they are assigned as they are created
+x = 2 #this is an integer
+y = 2.34 #this is a float
+z = 2j #this is a complex number type
+
+type(x) #verifys the type of the function
+
+#operators in python are very similar to other languages
+#some helpful operators are -= and +=
+
+
+#lists in python are not immutable and are not sets, meaning they can have repeated elements
+
+fruitList = ["apples", "kiwi", "orange"]
+print (fruitList)
+
+for x in fruitList:
+    print(x)
+
+#the output of this code will print each element
+
+# the break statement will stop the loop at a given element
+
+#funcitnons in pythoon are written like this
+def shoot():
+    print("Boom!")
+
+#you can add array elements with append() and remove with pop()
